619cc6b8fad568f620f2f08f383cc81aed20788d
youngchan-k <kyc9618@gmail.com>
2025-05-24 13:46:58 +0900

    Handle error of Polar

--- src/app/checkout/route.ts
+++ src/app/checkout/route.ts
@@ -3,5 +3,5 @@ import { Checkout } from "@polar-sh/nextjs";
 export const GET = Checkout({
   accessToken: process.env.POLAR_ACCESS_TOKEN!,
   successUrl: "/profile/credits?checkout_id={CHECKOUT_ID}&payment_success=true",
-  server: process.env.NODE_ENV === 'production' ? 'production' : 'sandbox',
-}); 
+  server: "production",
+});
